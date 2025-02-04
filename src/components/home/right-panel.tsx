2a6a498c644f506de5d928139b9d6749b79caf0f
imprince26 <princepatel260806@gmail.com>
2025-02-04 17:59:37 +0530

    fix layout

--- src/components/home/right-panel.tsx
+++ src/components/home/right-panel.tsx
@@ -9,49 +9,61 @@ import { useConversationStore } from "@/store/chat-store";
 import { useConvexAuth } from "convex/react";
 
 const RightPanel = () => {
-	const { selectedConversation, setSelectedConversation } = useConversationStore();
-	const { isLoading } = useConvexAuth();
+  const { selectedConversation, setSelectedConversation } =
+    useConversationStore();
+  const { isLoading } = useConvexAuth();
 
-	if (isLoading) return null;
-	if (!selectedConversation) return <ChatPlaceHolder />;
+  if (isLoading) return null;
+  if (!selectedConversation) return <ChatPlaceHolder />;
 
-	const conversationName = selectedConversation.groupName || selectedConversation.name;
-	const conversationImage = selectedConversation.groupImage || selectedConversation.image;
+  const conversationName =
+    selectedConversation.groupName || selectedConversation.name;
+  const conversationImage =
+    selectedConversation.groupImage || selectedConversation.image;
 
-	return (
-		<div className='w-3/4 flex flex-col'>
-			<div className='w-full sticky top-0 z-50'>
-				{/* Header */}
-				<div className='flex justify-between bg-gray-primary p-3'>
-					<div className='flex gap-3 items-center'>
-						<Avatar>
-							<AvatarImage src={conversationImage || "/placeholder.png"} className='object-cover' />
-							<AvatarFallback>
-								<div className='animate-pulse bg-gray-tertiary w-full h-full rounded-full' />
-							</AvatarFallback>
-						</Avatar>
-						<div className='flex flex-col'>
-							<p>{conversationName}</p>
-							{selectedConversation.isGroup && (
-								<GroupMembersDialog selectedConversation={selectedConversation} />
-							)}
-						</div>
-					</div>
+  return (
+    <div className="w-0 md:w-3/4  hidden md:flex flex-col ">
+      <div className="w-full sticky top-0 z-50">
+        {/* Header */}
+        <div className="flex justify-between bg-gray-primary p-3">
+          <div className="flex gap-3 items-center">
+            <Avatar>
+              <AvatarImage
+                src={conversationImage || "/placeholder.png"}
+                className="object-cover"
+              />
+              <AvatarFallback>
+                <div className="animate-pulse bg-gray-tertiary w-full h-full rounded-full" />
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <p>{conversationName}</p>
+              {selectedConversation.isGroup && (
+                <GroupMembersDialog
+                  selectedConversation={selectedConversation}
+                />
+              )}
+            </div>
+          </div>
 
-					<div className='flex items-center gap-7 mr-5'>
-						<a href='/video-call' target='_blank'>
-							<Video size={23} />
-						</a>
-						<X size={16} className='cursor-pointer' onClick={() => setSelectedConversation(null)} />
-					</div>
-				</div>
-			</div>
-			{/* CHAT MESSAGES */}
-			<MessageContainer />
+          <div className="flex items-center gap-7 mr-5">
+            <a href="/video-call" target="_blank">
+              <Video size={23} />
+            </a>
+            <X
+              size={16}
+              className="cursor-pointer"
+              onClick={() => setSelectedConversation(null)}
+            />
+          </div>
+        </div>
+      </div>
+      {/* CHAT MESSAGES */}
+      <MessageContainer />
 
-			{/* INPUT */}
-			<MessageInput />
-		</div>
-	);
+      {/* INPUT */}
+      <MessageInput />
+    </div>
+  );
 };
 export default RightPanel;
